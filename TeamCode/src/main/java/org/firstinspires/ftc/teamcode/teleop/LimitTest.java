ade4a914a13066b3015a33363805c8df9ea3aec1
Devin34 <halld35n@gmail.com>
2021-01-26 17:02:11 -0600

    feat(LimitTest.java): added to help determine positions manually

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/LimitTest.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.hardware.Robot;
+
+
+@TeleOp(name = "LimitTest", group = "Linear Opmode")
+public class LimitTest extends LinearOpMode {
+
+    Robot robot = new Robot();
+
+
+
+
+    @Override
+    public void runOpMode() {
+
+        robot.init(hardwareMap);
+
+        robot.wobbleArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //robot.wobbleArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        waitForStart();
+
+        while (!isStopRequested()) {
+
+
+
+            telemetry.addData("Motor Position", String.valueOf(robot.wobbleArm.getCurrentPosition()));
+            telemetry.addData("Servo Position", String.valueOf(robot.wobbleClaw.getPosition()));
+            telemetry.update();
+        }
+    }
+}
