079e35ea8bcf5600e8d956db363a6a46216946f2
Devin34 <halld35n@gmail.com>
2021-02-06 18:47:37 -0600

    feat: blue side auto
    As of the 2/6/21 meet

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/ParkAllBlue.java
@@ -0,0 +1,151 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.arcrobotics.ftclib.vision.UGContourRingPipeline;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.hardware.Robot;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+
+@Autonomous(name = "ParkAll Blue", group = "Autonomous Blue")
+public class ParkAllBlue extends LinearOpMode {
+    private static final int CAMERA_WIDTH = 320; // width  of wanted camera resolution
+    private static final int CAMERA_HEIGHT = 240; // height of wanted camera resolution
+
+    private static final int HORIZON = 100; // horizon value to tune
+
+    private static final boolean DEBUG = false; // if debug is wanted, change to true
+
+    private static final boolean USING_WEBCAM = true; // change to true if using webcam
+    private static final String WEBCAM_NAME = "webcam"; // insert webcam name from configuration if using webcam
+    Robot robot = new Robot();
+    private UGContourRingPipeline pipeline;
+    private OpenCvCamera camera;
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot.init(hardwareMap);
+
+        int cameraMonitorViewId = this
+                .hardwareMap
+                .appContext
+                .getResources().getIdentifier(
+                        "cameraMonitorViewId",
+                        "id",
+                        hardwareMap.appContext.getPackageName()
+                );
+        if (USING_WEBCAM) {
+            camera = OpenCvCameraFactory
+                    .getInstance()
+                    .createWebcam(hardwareMap.get(WebcamName.class, WEBCAM_NAME), cameraMonitorViewId);
+        } else {
+            camera = OpenCvCameraFactory
+                    .getInstance()
+                    .createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
+        }
+
+        camera.setPipeline(pipeline = new UGContourRingPipeline(telemetry, DEBUG));
+
+        UGContourRingPipeline.Config.setCAMERA_WIDTH(CAMERA_WIDTH);
+
+        UGContourRingPipeline.Config.setHORIZON(HORIZON);
+
+        camera.openCameraDeviceAsync(() -> camera.startStreaming(CAMERA_WIDTH, CAMERA_HEIGHT, OpenCvCameraRotation.UPRIGHT));
+
+
+        waitForStart();
+
+        sleep(3000);
+        String height = height = String.valueOf(pipeline.getHeight());
+        telemetry.addData("[Ring Stack] >>", height);
+        telemetry.update();
+
+        if (height.equals("ONE")) {  //Zone B
+
+            auto(10.0, 28.0, 0.0);
+        }
+
+
+        if (height.equals("FOUR")) {  //Zone C
+            auto(34.0, 54.0, 0.0);
+            SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+            Trajectory two = drive.trajectoryBuilder(new Pose2d(34.0, 54.0, 0.0))
+                    .lineToLinearHeading(new Pose2d(0.0, 54.0, 0.0))
+                    .build();
+
+            drive.setPoseEstimate(new Pose2d(-63.0, 36.0, Math.toRadians(180)));
+            drive.followTrajectory(two);
+
+
+        }
+
+        if (height.equals("ZERO")) { //Zone A
+
+                auto(0.0, 54.0, 0.0);
+
+        }
+
+
+    }
+
+    private void auto(double x, double y, double h) {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        Trajectory one = drive.trajectoryBuilder(new Pose2d(-63.0, 36.0, Math.toRadians(180)))
+                .splineToSplineHeading(new Pose2d(x, y), h)
+                .build();
+
+        drive.setPoseEstimate(new Pose2d(-63.0, 36.0, Math.toRadians(180)));
+
+
+        robot.shooterMotor.setPower(.58);
+
+
+        sleep(5000);
+
+
+        robot.intakeServo.setPower(1);
+
+        sleep(1000);
+
+        robot.intakeServo.setPower(0);
+
+        sleep(2000);
+
+        robot.intakeServo.setPower(1);
+
+        sleep(4000);
+
+        robot.shooterMotor.setPower(0);
+        robot.intakeServo.setPower(0);
+
+
+        drive.followTrajectory(one);
+
+        robot.wobbleArm.setPower(.5);
+
+        sleep(1000);
+        robot.wobbleArm.setPower(0);
+        robot.wobbleClaw.setPosition(.4);
+
+        sleep(500);
+    }
+
+
+
+}
+
+
+
+
+
+
